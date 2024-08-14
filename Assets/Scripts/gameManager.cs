6fa4c4385f9ae0f7ef879a8f77e701edc5020fd3
CoolBeansStudioOfficial <134745254+CoolBeansStudioOfficial@users.noreply.github.com>
2024-08-14 01:13:19 -0500

    The game loop and UI is complete

--- Assets/Scripts/gameManager.cs
+++ Assets/Scripts/gameManager.cs
@@ -2,37 +2,106 @@ using UnityEngine;
 
 public class gameManager : MonoBehaviour
 {
+    public GameObject canvas;
+    public GameObject crown;
+
     public Animator player;
     public Animator enemy;
 
-    public Animator playerRock;
-    public Animator playerPaper;
-    public Animator playerScissors;
+    public GameObject playerRock;
+    public GameObject playerPaper;
+    public GameObject playerScissors;
 
-    public Animator enemyRock;
-    public Animator enemyPaper;
-    public Animator enemyScissors;
+    public GameObject enemyRock;
+    public GameObject enemyPaper;
+    public GameObject enemyScissors;
 
-    public void StartGame(int playerChoice)
+    int playerChoice;
+    int enemyChoice;
+
+    public void StartGame(int pc)
     {
-        int enemyChoice = Random.Range(0, 3);
+        playerChoice = pc;
+        enemyChoice = Random.Range(0, 3);
+
+        Animate(playerChoice, enemyChoice);
+
+        
+    }
+
+    void Animate(int pc, int ec)
+    {
+        canvas.SetActive(false);
+
+        player.enabled = true;
+        enemy.enabled = true;
+
+        if (pc == 0)
+        {
+            playerRock.SetActive(true);
+        }
+        else if (pc == 1)
+        { 
+            playerPaper.SetActive(true);
+        }
+        else
+        {
+            playerScissors.SetActive(true);
+        }
+
+        if (ec == 0)
+        {
+            enemyRock.SetActive(true);
+        }
+        else if (ec == 1)
+        {
+            enemyPaper.SetActive(true);
+        }
+        else
+        {
+            enemyScissors.SetActive(true);
+        }
+
+        Invoke("ShowWinner", 1f);
+        Invoke("HideWinner", 3f);
+        Invoke("FinishAnimation", 4f);
+
+        
+    }
+
+    void FinishAnimation()
+    {
+        player.enabled = false;
+        enemy.enabled = false;
+
+        playerRock.SetActive(false);
+        playerPaper.SetActive(false);
+        playerScissors.SetActive(false);
+
+        enemyRock.SetActive(false);
+        enemyPaper.SetActive(false);
+        enemyScissors.SetActive(false);
+
+        canvas.SetActive(true);
+    }
+
+    void ShowWinner()
+    {
+        string winState;
 
         if (playerChoice == 0)
         {
             if (enemyChoice == 0)
             {
-                player.enabled = true;
-                enemy.enabled = true;
+                winState = "tie";
             }
             else if (enemyChoice == 1)
             {
-                player.enabled = true;
-                enemy.enabled = true;
+                winState = "lose";
             }
             else
             {
-                player.enabled = true;
-                enemy.enabled = true;
+                winState = "win";
             }
         }
 
@@ -40,18 +109,15 @@ public class gameManager : MonoBehaviour
         {
             if (enemyChoice == 0)
             {
-                player.enabled = true;
-                enemy.enabled = true;
+                winState = "win";
             }
             else if (enemyChoice == 1)
             {
-                player.enabled = true;
-                enemy.enabled = true;
+                winState = "tie";
             }
             else
             {
-                player.enabled = true;
-                enemy.enabled = true;
+                winState = "lose";
             }
         }
 
@@ -59,19 +125,36 @@ public class gameManager : MonoBehaviour
         {
             if (enemyChoice == 0)
             {
-                player.enabled = true;
-                enemy.enabled = true;
+                winState = "lose";
             }
             else if (enemyChoice == 1)
             {
-                player.enabled = true;
-                enemy.enabled = true;
+                winState = "win";
             }
             else
             {
-                player.enabled = true;
-                enemy.enabled = true;
+                winState = "tie";
             }
         }
+
+        if (winState == "win")
+        {
+            crown.transform.position = new Vector2(-4.25f, 1f);
+            crown.SetActive(true);
+        }
+        else if (winState == "lose")
+        {
+            crown.transform.position = new Vector2(4.25f, 1f);
+            crown.SetActive(true);
+        }
+        else
+        {
+            crown.SetActive(false);
+        }
+    }
+
+    void HideWinner()
+    { 
+        crown.SetActive(false); 
     }
 }
